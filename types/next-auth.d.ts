7c14850edbf5b7ccc74ab3afe09ff2a691dab811
Mnigos <makowskiigor@gmail.com>
2024-08-11 13:49:28 +0200

    feat(auth/config): add callbacks to insert `id` into `User` session object

---
+++ types/next-auth.d.ts
@@ -0,0 +1,18 @@
+import type { DefaultSession } from 'next-auth'
+import type { JWT as DefaultJWT } from 'next-auth/jwt'
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT extends DefaultJWT {
+    /** OpenID ID Token */
+    id: string
+  }
+}
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    token: {
+      id: string
+    }
+  }
+}

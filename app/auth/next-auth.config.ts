07fba56b8fbc1acb9abe2028c6fb43088ab6b544
Mnigos <makowskiigor@gmail.com>
2024-08-11 13:49:28 +0200

    feat(auth/config): add callbacks to insert `id` into `User` session object

--- app/auth/next-auth.config.ts
+++ app/auth/next-auth.config.ts
@@ -3,4 +3,21 @@ import Discord from 'next-auth/providers/discord'
 
 export const nextAuthConfig: NextAuthConfig = {
   providers: [Discord],
+  callbacks: {
+    jwt({ token, user }) {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (user?.id) {
+        token.id = user.id
+      }
+
+      return token
+    },
+    session({ session, token }) {
+      if (token.sub) {
+        session.user.id = token.sub
+      }
+
+      return session
+    },
+  },
 }
--- app/auth/next-auth.ts
+++ app/auth/next-auth.ts
@@ -3,7 +3,7 @@ import NextAuth from 'next-auth'
 
 import { nextAuthConfig } from './next-auth.config'
 
-import prisma from '@app/db'
+import { prisma } from '@app/db'
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),

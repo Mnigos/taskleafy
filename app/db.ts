b23aa171c9a76e78337b74ed0ec7b62ebf53e404
Mnigos <makowskiigor@gmail.com>
2024-08-11 13:47:53 +0200

    refactor(db): remove unnecessary complexity

--- app/db.ts
+++ app/db.ts
@@ -1,15 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
 declare const globalThis: {
-  prismaGlobal?: ReturnType<typeof prismaClientSingleton>
+  prismaGlobal?: PrismaClient
 } & typeof global
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
+export const prisma = globalThis.prismaGlobal ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma

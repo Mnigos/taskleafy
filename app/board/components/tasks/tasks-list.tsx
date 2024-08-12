507936853ebafa15c7eb1578b76ec94f3eb748d4
Mnigos <makowskiigor@gmail.com>
2024-08-12 09:13:07 +0200

    feat(board/components/tasks/list): create tasks list

---
+++ app/board/components/tasks/tasks-list.tsx
@@ -0,0 +1,34 @@
+import { TaskCard } from './task-card'
+
+import type { PickedTask } from '@app/board/types'
+
+namespace TasksList {
+  export type Props = Readonly<{
+    tasks: PickedTask[]
+    header: string
+  }>
+}
+
+function TasksList({ tasks, header }: TasksList.Props) {
+  return (
+    <div className="flex flex-col gap-2">
+      <header>
+        <h2 className="text-xl">{header}</h2>
+      </header>
+
+      <div>{`${tasks.length} ${tasks.length === 1 ? 'task' : 'tasks'}`}</div>
+
+      {tasks.map(({ id, name, isDone, dueDate }) => (
+        <TaskCard
+          key={id}
+          id={id}
+          name={name}
+          isDone={isDone}
+          dueDate={dueDate}
+        />
+      ))}
+    </div>
+  )
+}
+
+export { TasksList }

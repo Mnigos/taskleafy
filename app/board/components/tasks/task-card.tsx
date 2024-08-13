5b5c2babdd48a9c83d48af6af88aad42028412cc
Mnigos <makowskiigor@gmail.com>
2024-08-13 15:49:34 +0200

    feat(board/components/task-card): wrap it with `Draggable`

--- app/board/components/tasks/task-card.tsx
+++ app/board/components/tasks/task-card.tsx
@@ -1,52 +1,90 @@
 'use client'
 
 import { fromDate, getLocalTimeZone } from '@internationalized/date'
+import { Draggable } from 'react-beautiful-dnd'
 import { Card, CardHeader } from '@nextui-org/card'
 import { Checkbox } from '@nextui-org/checkbox'
 import { cn } from '@nextui-org/theme'
 import { LuCalendarDays } from 'react-icons/lu'
 
-import { markTaskAsDoneAction } from '@app/board/actions'
 import { formatDateValue, isOverdue } from '@app/board/helpers/date'
-import type { PickedTask } from '@app/board/types'
+import type { BoardKeyWithoutOverdue, PickedTask } from '@app/board/types'
+import { useTasksBoard } from '@app/board/context'
 
-export function TaskCard({ id, name, isDone, dueDate }: Readonly<PickedTask>) {
+namespace TaskCard {
+  export type Props = Readonly<
+    PickedTask & {
+      index: number
+    }
+  >
+}
+
+function TaskCard({ id, name, isDone, dueDate, index }: TaskCard.Props) {
+  const { tasksBoard, changeTaskTable } = useTasksBoard()
+
   const dueDateValue = dueDate
     ? fromDate(dueDate, getLocalTimeZone())
     : undefined
 
   return (
-    <Card>
-      <CardHeader
-        className={cn(
-          isDone && 'opacity-50',
-          'flex flex-col items-start gap-1'
-        )}
-      >
-        <div>
-          <Checkbox
-            color="primary"
-            defaultSelected={isDone}
-            isDisabled={isDone}
-            onClick={() => !isDone && markTaskAsDoneAction(id)}
-          />
-
-          {name}
-        </div>
-
-        {dueDate && dueDateValue && !isDone && (
-          <div
+    <Draggable draggableId={id} index={index}>
+      {(
+        { innerRef, draggableProps, dragHandleProps },
+        { isDragging },
+        { source: { droppableId } }
+      ) => (
+        <Card
+          ref={innerRef}
+          {...draggableProps}
+          {...dragHandleProps}
+          className={cn(isDragging && 'opacity-50')}
+        >
+          <CardHeader
             className={cn(
-              'flex items-center gap-1 text-xs',
-              isOverdue(dueDateValue) ? 'text-danger' : 'text-primary'
+              isDone && 'opacity-50',
+              'flex flex-col items-start gap-1'
             )}
           >
-            <LuCalendarDays />
+            <div>
+              <Checkbox
+                color="primary"
+                defaultSelected={isDone}
+                isDisabled={isDone}
+                onClick={() => {
+                  console.log(droppableId)
 
-            {formatDateValue(dueDateValue)}
-          </div>
-        )}
-      </CardHeader>
-    </Card>
+                  const sourceKey = droppableId as BoardKeyWithoutOverdue
+                  const task = tasksBoard[sourceKey].items.find(
+                    task => task.id === id
+                  )
+
+                  if (!isDone && task)
+                    setTimeout(() => {
+                      changeTaskTable(task, sourceKey, 'done')
+                    }, 500)
+                }}
+              />
+
+              {name}
+            </div>
+
+            {dueDate && dueDateValue && !isDone && (
+              <div
+                className={cn(
+                  'flex items-center gap-1 text-xs',
+                  isOverdue(dueDateValue) ? 'text-danger' : 'text-primary'
+                )}
+              >
+                <LuCalendarDays />
+
+                {formatDateValue(dueDateValue)}
+              </div>
+            )}
+          </CardHeader>
+        </Card>
+      )}
+    </Draggable>
   )
 }
+
+export { TaskCard }

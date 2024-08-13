229e8f1c1028674aba37dabe9304417dc8834673
Mnigos <makowskiigor@gmail.com>
2024-08-13 15:44:23 +0200

    refactor(board/actions): move `AddTask` type to `types/task`

--- app/board/types/picked-task.ts
+++
@@ -1,7 +0,0 @@
-import type { Task } from '@prisma/client'
-import type { SetOptional } from 'type-fest'
-
-export type PickedTask = SetOptional<
-  Pick<Task, 'id' | 'name' | 'isDone' | 'dueDate'>,
-  'dueDate'
->

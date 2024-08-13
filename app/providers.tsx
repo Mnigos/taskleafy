35d4b8e997ee557a0596f3a16d81504d4811a1b7
Mnigos <makowskiigor@gmail.com>
2024-08-13 14:28:40 +0200

    feat(providers): setup `QueryClientProvider`

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,26 @@
 'use client'
 
 import { NextUIProvider } from '@nextui-org/system'
+import {
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { useRouter } from 'next/navigation'
-import type { ReactNode } from 'react'
+import { useState, type ReactNode } from 'react'
 
 export function RootProviders({ children }: Readonly<{ children: ReactNode }>) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        mutationCache: new MutationCache(),
+      })
+  )
   const router = useRouter()
 
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+    </QueryClientProvider>
+  )
 }

e60bb045a263e92cdeee1c603974939246e8ec62
Mnigos <makowskiigor@gmail.com>
2024-08-15 09:36:12 +0200

    feat(board/components/tasks): create `RescheduleOverdueTasks`

---
+++ app/board/components/tasks/reschedule-overdue-tasks.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import type { DateValue } from '@internationalized/date'
+import { Button } from '@nextui-org/button'
+import { Calendar } from '@nextui-org/calendar'
+import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/popover'
+import { useEffect, useState } from 'react'
+import { LuCalendarClock } from 'react-icons/lu'
+
+import { CalendarPresets } from '../misc'
+
+import { now } from '@app/board/helpers/date'
+import { useTasksBoard } from '@app/board/context'
+
+export function RescheduleOverdueTasks() {
+  const [dueDateValue, setDueDateValue] = useState<DateValue | undefined>()
+  const { rescheduleOverdueTasks } = useTasksBoard()
+
+  useEffect(() => {
+    if (dueDateValue) {
+      rescheduleOverdueTasks(dueDateValue).catch((error: unknown) => {
+        console.error(error)
+      })
+      setDueDateValue(undefined)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dueDateValue])
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button variant="ghost" color="primary">
+          <LuCalendarClock />
+          Reschedule
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent>
+        <Calendar
+          nextButtonProps={{ variant: 'bordered' }}
+          prevButtonProps={{ variant: 'bordered' }}
+          aria-label="Due date "
+          value={dueDateValue}
+          minValue={now}
+          topContent={
+            <CalendarPresets value={dueDateValue} setValue={setDueDateValue} />
+          }
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}

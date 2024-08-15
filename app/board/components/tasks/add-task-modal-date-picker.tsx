8e2c3558c0c72bc5446b37037e278c5e8e0ac70c
Mnigos <makowskiigor@gmail.com>
2024-08-15 09:35:43 +0200

    refactor(board/components/tasks): move `CalendarPresets` to `/misc`

--- app/board/components/tasks/add-task-modal-date-picker.tsx
+++ app/board/components/tasks/add-task-modal-date-picker.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { type DateValue } from '@internationalized/date'
-import { Button, ButtonGroup } from '@nextui-org/button'
+import { Button } from '@nextui-org/button'
 import { DatePicker } from '@nextui-org/date-picker'
 import { useState } from 'react'
 import { LuXCircle } from 'react-icons/lu'
 
-import { now, tomorrow, nextWeek } from '@app/board/helpers/date'
+import { CalendarPresets } from '../misc'
+
+import { now } from '@app/board/helpers/date'
 
 namespace AddTaskModalDatePicker {
   export type Props = Readonly<{
@@ -54,48 +56,7 @@ function AddTaskModalDatePicker({
         </Button>
       }
       CalendarTopContent={
-        <ButtonGroup
-          fullWidth
-          size="sm"
-          radius="full"
-          variant="bordered"
-          className="bg-content1 px-2 pt-2"
-        >
-          <Button
-            onPress={() => {
-              setDueDateValue(now)
-            }}
-            variant={
-              dueDateValue?.toString() === now.toString() ? 'solid' : 'bordered'
-            }
-          >
-            Today
-          </Button>
-          <Button
-            onPress={() => {
-              setDueDateValue(tomorrow)
-            }}
-            variant={
-              dueDateValue?.toString() === tomorrow.toString()
-                ? 'solid'
-                : 'bordered'
-            }
-          >
-            Tomorrow
-          </Button>
-          <Button
-            onPress={() => {
-              setDueDateValue(nextWeek)
-            }}
-            variant={
-              dueDateValue?.toString() === nextWeek.toString()
-                ? 'solid'
-                : 'bordered'
-            }
-          >
-            Next week
-          </Button>
-        </ButtonGroup>
+        <CalendarPresets value={dueDateValue} setValue={setDueDateValue} />
       }
     />
   )
